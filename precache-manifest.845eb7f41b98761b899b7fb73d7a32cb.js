a220f016c5673d9a6f7cd6804f361cdc419d8ec9
adpdocs <pmg2010@gmail.com>
2024-06-26 18:14:33 +0200

    Updates

---
+++ precache-manifest.845eb7f41b98761b899b7fb73d7a32cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2c6e0793e9b3dba80bb4919c45962b",
+    "url": "/livia/index.html"
+  },
+  {
+    "revision": "998a385edff5703a0ed9",
+    "url": "/livia/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cc7d5dc9e3beb887d2a9",
+    "url": "/livia/static/js/2.6508007a.chunk.js"
+  },
+  {
+    "revision": "998a385edff5703a0ed9",
+    "url": "/livia/static/js/main.91b5aa4e.chunk.js"
+  },
+  {
+    "revision": "fb1b74e75a3d160bd8d5",
+    "url": "/livia/static/js/runtime~main.d7c45a6c.js"
+  }
+]);
