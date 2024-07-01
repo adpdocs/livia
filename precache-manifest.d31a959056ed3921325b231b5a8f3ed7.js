7ddaf80e863329f297dd0a861f16927d52f0e621
adpdocs <pmg2010@gmail.com>
2024-07-01 16:47:17 +0200

    Updates

---
+++ precache-manifest.d31a959056ed3921325b231b5a8f3ed7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b6398d3739be46a256433d7ad4916c",
+    "url": "/livia/index.html"
+  },
+  {
+    "revision": "3991238069ba00a6b115",
+    "url": "/livia/static/css/main.780c4ad6.chunk.css"
+  },
+  {
+    "revision": "4c2134abf6c23b717a2c",
+    "url": "/livia/static/js/2.3b066323.chunk.js"
+  },
+  {
+    "revision": "800ad8f1c2aac2d1e1b1",
+    "url": "/livia/static/js/3.340131de.chunk.js"
+  },
+  {
+    "revision": "49011b6d19aaf6ab5989",
+    "url": "/livia/static/js/4.13a843a3.chunk.js"
+  },
+  {
+    "revision": "3991238069ba00a6b115",
+    "url": "/livia/static/js/main.8b6a407e.chunk.js"
+  },
+  {
+    "revision": "d7704ed4c5d9ac7d7cfd",
+    "url": "/livia/static/js/runtime~main.0eebec5a.js"
+  }
+]);
