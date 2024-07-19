e9533238f1a1700ec34e2723d1f04821f49d81e0
adpdocs <pmg2010@gmail.com>
2024-07-19 09:50:08 +0200

    Updates

---
+++ precache-manifest.f6671a43a563157afaaa0a1e3bb9291c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb884d11e257d4aa3f1eb2fbd00b837",
+    "url": "/livia/index.html"
+  },
+  {
+    "revision": "535f02f507f58c60c335",
+    "url": "/livia/static/css/main.716a6dc2.chunk.css"
+  },
+  {
+    "revision": "816fddfb737dfcf8a725",
+    "url": "/livia/static/js/2.27fdae9b.chunk.js"
+  },
+  {
+    "revision": "76d62aa3d3e8ff410dc9",
+    "url": "/livia/static/js/3.2cc7dfbb.chunk.js"
+  },
+  {
+    "revision": "c1f76228a6e5963fd20e",
+    "url": "/livia/static/js/4.6da60bb7.chunk.js"
+  },
+  {
+    "revision": "d47286c8f5f505d209e0",
+    "url": "/livia/static/js/5.93c58faf.chunk.js"
+  },
+  {
+    "revision": "db2492d0d4d135d267c9",
+    "url": "/livia/static/js/6.b1c30f6e.chunk.js"
+  },
+  {
+    "revision": "8d8eb5e1a971406f0041",
+    "url": "/livia/static/js/7.92129dce.chunk.js"
+  },
+  {
+    "revision": "4abcebdba24af6b1f06e",
+    "url": "/livia/static/js/8.abf0a458.chunk.js"
+  },
+  {
+    "revision": "535f02f507f58c60c335",
+    "url": "/livia/static/js/main.853b2215.chunk.js"
+  },
+  {
+    "revision": "e040e544449393973b0f",
+    "url": "/livia/static/js/runtime~main.bd4d4c6c.js"
+  }
+]);
